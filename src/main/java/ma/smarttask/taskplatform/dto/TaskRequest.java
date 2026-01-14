508c95405c3e88497fbab2c79ad61141f68d3db2
ayalkadimail <al.kadiriaya.502@gmail.com>
2026-01-14 04:55:54 +0100

    add Dto + Validation

---
+++ src/main/java/ma/smarttask/taskplatform/dto/TaskRequest.java
@@ -0,0 +1,31 @@
+package ma.smarttask.taskplatform.dto;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import ma.smarttask.taskplatform.model.enums.Category;
+import ma.smarttask.taskplatform.model.enums.Difficulty;
+import ma.smarttask.taskplatform.model.enums.Priority;
+import ma.smarttask.taskplatform.model.enums.Topic;
+
+import java.time.LocalDate;
+
+public record TaskRequest(
+        @NotBlank(message = "Le titre est obligatoire")
+        String title,
+
+        String description,
+
+        @NotNull(message = "La priorite est obligatoire")
+        Priority priority,
+
+        @FutureOrPresent(message = "La date ne peut pas etre dans le passe")
+        LocalDate dueDate,
+
+        String type, // "GENERAL" ou "STUDY"
+
+        // Champs optionnels selon le type
+        Category category,
+        Topic topic,
+        Difficulty difficulty
+) {}

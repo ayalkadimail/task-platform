3591f8cee81438985c8e45d1757eff34a3e7978d
ayalkadimail <al.kadiriaya.502@gmail.com>
2026-01-15 18:12:56 +0100

    modif2

--- src/main/java/ma/smarttask/taskplatform/service/TaskService.java
+++ src/main/java/ma/smarttask/taskplatform/service/TaskService.java
@@ -6,10 +6,10 @@ import ma.smarttask.taskplatform.model.StudyTask;
 import ma.smarttask.taskplatform.model.enums.Topic;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
 public interface TaskService {
     AbstractTask findById(Long id);
     List<AbstractTask> findAll();
